ee1e6a1860770a5c82ba376779ddd24fdca05337
Abijit95 <abijitsaha20@iiitmk.ac.in>
2023-11-01 11:42:05 +0530

    Add New Loging Features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AnimationsComponent } from './animations/animations.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimationsComponent,
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
